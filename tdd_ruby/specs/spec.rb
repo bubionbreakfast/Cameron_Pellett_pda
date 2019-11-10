9b3576b55dbccaea746e26ad7aad1029a80d23c9
Cameron Pellett <bubionbreakfast@gmail.com>
2019-11-10 17:20:49 +0000

    Update spec.rb

--- tdd_ruby/specs/spec.rb
+++ tdd_ruby/specs/spec.rb
@@ -7,26 +7,29 @@ require('pry')
 
 class TestCardGame < MiniTest::Test
 
+ def setup
+    @card1 = Card.new('spades', 1)
+    @card2 = Card.new('hearts', 7)
 
+    @cards = [@card1, @card2]
+  end
 
-# def test_check_for_ace()
-#   card = Card.new('hearts', 1)
-#   assert_equal(1, card.value())
-# end
+  def test_check_for_ace__true()
+    assert_equal(true, CardGame.check_for_ace(@card1))
+  end
 
-# def test_highest_card()
-#   card1 = Card.new('hearts', 2)
-#   card2 = Card.new('spades', 7)
-#   # binding.pry
-#   assert_equal(card2, highest_card(card1, card2))
-# end
+  def test_check_for_ace__false()
+    assert_equal(false, CardGame.check_for_ace(@card2))
+  end
 
-def test_cards_total()
-  card1 = Card.new('hearts', 1)
-  card2 = Card.new('spades', 2)
-  card3 = Card.new('diamonds', 3)
-  cards = [card1, card2, card3]
-  assert_equal(6, cards_total(cards))
-end
+  def test_highest_card()
+    result = CardGame.highest_card(@card1, @card2)
+    assert_equal(@card2 , result)
+  end
+
+  def test_cards_total()
+    result = CardGame.cards_total(@cards)
+    assert_equal("You have a total of 8", result)
+  end
 
 end
